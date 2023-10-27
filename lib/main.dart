8e6e58e4bacec2e4f9218e07ff5227e746105ef0
uni51 <unieye51@gmail.com>
2023-10-27 11:55:39 +0900

    コードをスッキリさせる2

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
@@ -43,7 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('riverpodサンプル'),
       ),
       body: Center(
         child: Column(
