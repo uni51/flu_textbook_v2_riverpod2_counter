953535202c55920d8de49c7f2d66d9ae54b04ef4
uni51 <unieye51@gmail.com>
2023-10-31 07:40:55 +0900

    CosumerWidgetの細分化

--- lib/main.dart
+++ lib/main.dart
@@ -23,74 +23,92 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends ConsumerWidget {
-
+class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final int state = ref.watch(counterProvider);
-    final Color color = ref.watch(randomColorProvider);
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$state',
-              style: TextStyle(
-                color: color,
-                fontSize: 64,
-              ),
-            ),
+            CounterText(),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              final notifier = ref.read(randomColorProvider.notifier);
-              notifier.randomColor();
-            },
-            child: const Icon(Icons.color_lens),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              final notifier = ref.read(counterProvider.notifier);
-              notifier.resetCounter();
-            },
-            child: const Icon(Icons.exposure_zero),
-          ),
-          FloatingActionButton(
-            onPressed: (){
-              final notifier = ref.read(counterProvider.notifier);
-              notifier.decrementCounter();
-            },
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: (){
-              final notifier = ref.read(counterProvider.notifier);
-              notifier.incrementCounter();
-              // ref.read(counterProvider.notifier).incrementCounter();
-            },
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              final notifier = ref.read(counterProvider.notifier);
-              notifier.multiplyCounter();
-            },
-            child: const Icon(Icons.close),
-          ),
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton:
+          const Buttons(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CounterText extends ConsumerWidget {
+  const CounterText({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+    final Color color = ref.watch(randomColorProvider);
+    return Text(
+      '$counter',
+      style: TextStyle(
+        color: color,
+        fontSize: 64,
+      ),
+    );
+  }
+}
+
+class Buttons extends ConsumerWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            final notifier = ref.read(randomColorProvider.notifier);
+            notifier.randomColor();
+          },
+          child: const Icon(Icons.color_lens),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            final notifier = ref.read(counterProvider.notifier);
+            notifier.resetCounter();
+          },
+          child: const Icon(Icons.exposure_zero),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            final notifier = ref.read(counterProvider.notifier);
+            notifier.decrementCounter();
+          },
+          child: const Icon(Icons.remove),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            final notifier = ref.read(counterProvider.notifier);
+            notifier.incrementCounter();
+            // ref.read(counterProvider.notifier).incrementCounter();
+          },
+          child: const Icon(Icons.add),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            final notifier = ref.read(counterProvider.notifier);
+            notifier.multiplyCounter();
+          },
+          child: const Icon(Icons.close),
+        ),
+      ],
     );
   }
 }
